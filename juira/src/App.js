e757e09da37ce95cc2a858326aab01ebef37be5d
Jeronimo Cardozo <jdcm182@gmail.com>
2022-10-15 02:05:40 -0300

    Add style to Landing.jsx

--- juira/src/App.js
+++ juira/src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Route exact path={'/'} component={Landing} />
+      <Route path={'/juira'} component={Landing} />
     </div>
   );
 }
--- juira/src/components/Landing.jsx
+++ juira/src/components/Landing.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import styles from './Landing.module.css';
-//import
+import logo from './media/juira_white.png';
+
 
 export default function Landing() {
     return (
-        <section className={styles.wrapper}>
-            LANDING!
+        <section className={styles.container}>
+            <div className={styles.box}>
+                <div className={styles.imgContainer}>
+                    <img src={logo} alt="Juira logo"
+                        className={styles.logo}></img>
+                </div>
+                <br />
+                <a href="/juira" className={styles.btnEntrar}>Ingresar</a>
+            </div>
+            <div className={styles.imgWrapper}>
+                <div className={styles.imgPattern}>
+                </div>
+            </div>
         </section>
     )
 }
