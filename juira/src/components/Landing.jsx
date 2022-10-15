d6917008315876f572c31b00c6b93b46dedb7fc4
Jeronimo Cardozo <jdcm182@gmail.com>
2022-10-14 23:22:04 -0300

    Add router-dom first path /
    Initialize redux, react-redux, redux-thunk, react-router-dom
    
    Create first component, Landing.jsx

---
+++ juira/src/components/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Landing.module.css';
+//import
+
+export default function Landing() {
+    return (
+        <section className={styles.wrapper}>
+            LANDING!
+        </section>
+    )
+}
