786956d0f6fdc502b175c2bdabc1ff3ad8c2882c
Jonatan P <jonatan2502@gmail.com>
2022-10-15 21:50:48 -0400

    pagination component

--- juira/src/components/Pagination/Pagination.jsx
+++ juira/src/components/Pagination/Pagination.jsx
@@ -2,10 +2,14 @@ import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 
-export default function Pagination({ totalPages }) {
+function handleChange(e) {
+  //Funcion que setea el rango de productos a mostrar -por desarrollar-
+  console.log(e)
+}
+export default function PaginationButtons({ totalPages }) {
   return (
     <Stack spacing={2}>
-      <Pagination count={totalPages} onChange={ e => handleChange(e)} showFirstButton showLastButton />
+      <Pagination count={totalPages} onChange={(event, page) => handleChange(page)} showFirstButton showLastButton />
     </Stack>
   );
 }
