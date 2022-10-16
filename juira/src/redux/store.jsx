5687eeb3f5295299cd1160e29caa914e6be462df
Mariana Salez <mariana.i.salez@gmail.com>
2022-10-16 20:40:44 -0300

    fix redux

--- juira/src/redux/store.jsx
+++ juira/src/redux/store.jsx
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware,compose } from 'redux';
 import thunk from 'redux-thunk';
 import { rootReducer } from './reducers';
 
-const middleware = applyMiddleware(thunk);
-export const store = createStore(rootReducer, middleware);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+export const store = createStore(rootReducer,composeEnhancer(applyMiddleware(thunk)));
