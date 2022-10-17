780c3622c5fe6c3adb496d74ca0e9d652cc98dde
arturoUrgel <arturo.urgel@gmail.com>
2022-10-17 16:50:32 -0300

    sortAndFilterHome

--- juira/src/components/CardsGrid/CardsGrid.jsx
+++ juira/src/components/CardsGrid/CardsGrid.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
-import styles from './CardsGrid.module.css';
-import Card from '../Card/Card.jsx'
-
+import React from "react";
+import styles from "./CardsGrid.module.css";
+import Card from "../Card/Card.jsx";
 
 export default function CardsGrid({ products }) {
-    return (
-        <div className={styles.container/* cards_grid_container */}>
-            <div className={styles.product_grid}>
-
-
-                {products && products.map(p => (
-                    <Card key={'crd' + p.id} className={styles.cardItem}
-                        id={p.id} price={p.price} name={p.name} image={p.image} />
-                ))}
-
-            </div>
-        </div >
-    )
+  return (
+    <div className={styles.container /* cards_grid_container */}>
+      <div className={styles.product_grid}>
+        {products &&
+          products.map((p) => (
+            <Card
+              key={"crd" + p.id}
+              className={styles.cardItem}
+              id={p.id}
+              price={p.price}
+              name={p.name}
+              image={p.image}
+            />
+          ))}
+      </div>
+    </div>
+  );
 }
