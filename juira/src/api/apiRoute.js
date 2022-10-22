f8e05d6e13ba784cabd0d2f00a117b87cbc98129
Mariana Salez <mariana.i.salez@gmail.com>
2022-10-22 16:06:28 -0300

    fix route

--- juira/src/api/apiRoute.js
+++ juira/src/api/apiRoute.js
@@ -5,7 +5,7 @@ export const getProductsDetailsApi = '/product-detail/'
 export const getCategoriesNameApi = "categories/names"
 export const getCategoriesIdApi = "categories/"
 
-export const API_URL_BACKEND = 'https://pf-henry-pt07g06-back-production.up.railway.app/'
+export const API_URL_BACKEND = 'https://pf-henry-pt07g06-back-production.up.railway.app'
 
 //'https://containers-us-west-65.railway.app'
 //https://pf-henry-pt07g06-back-production.up.railway.app/
