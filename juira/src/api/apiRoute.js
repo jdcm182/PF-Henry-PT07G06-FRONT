bad2ab15b97211d8dc4248e02e454f56f64ccd48
Mariana Salez <mariana.i.salez@gmail.com>
2022-10-22 15:24:35 -0300

    cambio de api para que funcione

--- juira/src/api/apiRoute.js
+++ juira/src/api/apiRoute.js
@@ -5,4 +5,7 @@ export const getProductsDetailsApi = '/product-detail/'
 export const getCategoriesNameApi = "categories/names"
 export const getCategoriesIdApi = "categories/"
 
-export const API_URL_BACKEND = 'http://localhost:3001/'
+export const API_URL_BACKEND = 'https://pf-henry-pt07g06-back-production.up.railway.app/'
+
+//'https://containers-us-west-65.railway.app'
+//https://pf-henry-pt07g06-back-production.up.railway.app/
