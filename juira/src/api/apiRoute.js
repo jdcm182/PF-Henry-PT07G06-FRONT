7adf43db2ecc1bf3e8dff41861f8ed979dff803d
Mariana Salez <mariana.i.salez@gmail.com>
2022-10-22 15:51:29 -0300

    changes to run

--- juira/src/api/apiRoute.js
+++ juira/src/api/apiRoute.js
@@ -5,4 +5,4 @@ export const getProductsDetailsApi = '/product-detail/'
 export const getCategoriesNameApi = "categories/names"
 export const getCategoriesIdApi = "categories/"
 
-export const API_URL_BACKEND = 'https://pf-henry-pt07g06-back-production.up.railway.app/'
+export const API_URL_BACKEND = process.env.REACT_APP_API || 'http://localhost:3001/'
