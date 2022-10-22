aa03e69515d5d1c85f4c22c8be42c82c306145a3
Mariana Salez <mariana.i.salez@gmail.com>
2022-10-22 16:15:26 -0300

    back changes

--- juira/src/api/apiRoute.js
+++ juira/src/api/apiRoute.js
@@ -1,9 +1,9 @@
 // product
-export const getAllProductsApi = '/products/'
+export const getAllProductsApi = 'products/'
 /* export const getAllProductsApi = 'products?status=Publicado' */
 export const getProductsDetailsApi = '/product-detail/'
-export const getCategoriesNameApi = "/categories/names"
-export const getCategoriesIdApi = "/categories/"
+export const getCategoriesNameApi = "categories/names"
+export const getCategoriesIdApi = "categories/"
 
 export const API_URL_BACKEND = process.env.REACT_APP_API || 'http://localhost:3001/'
 
