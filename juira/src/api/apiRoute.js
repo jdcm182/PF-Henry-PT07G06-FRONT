c5a49495b788bb7a7912631969ca843032434a4b
Mariana Salez <mariana.i.salez@gmail.com>
2022-10-22 16:02:33 -0300

    update back url

--- juira/src/api/apiRoute.js
+++ juira/src/api/apiRoute.js
@@ -5,4 +5,4 @@ export const getProductsDetailsApi = '/product-detail/'
 export const getCategoriesNameApi = "categories/names"
 export const getCategoriesIdApi = "categories/"
 
-export const API_URL_BACKEND = 'http://localhost:3001/'
+export const API_URL_BACKEND = 'https://pf-henry-pt07g06-back-production.up.railway.app/'
