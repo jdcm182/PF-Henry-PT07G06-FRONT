c07688328847cdea682a5085610397bb7ce1040e
Mariana Salez <mariana.i.salez@gmail.com>
2022-10-22 21:30:19 -0300

    fix back call

--- juira/src/api/apiRoute.js
+++ juira/src/api/apiRoute.js
@@ -5,9 +5,9 @@ export const getProductsDetailsApi = '/product-detail/'
 export const getCategoriesNameApi = "categories/names"
 export const getCategoriesIdApi = "categories/"
 
-export const API_URL_BACKEND = 'https://pf-henry-pt07g06-back-production.up.railway.app/'
+export const API_URL_BACKEND = process.env.REACT_APP_API  || 'http://localhost:3001/'
 
-// process.env.REACT_APP_API  || 'http://localhost:3001/'
+// 'https://pf-henry-pt07g06-back-production.up.railway.app/'
 
 
 //'https://containers-us-west-65.railway.app'
