121475efee433d092dde8f77ed83d606da9ad45d
arturoUrgel <arturo.urgel@gmail.com>
2022-10-24 01:59:45 -0300

    MercadoPago

--- juira/src/components/ShoppingCart/ShoppingCart.jsx
+++ juira/src/components/ShoppingCart/ShoppingCart.jsx
@@ -5,7 +5,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Typography, Container } from "@mui/material";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
@@ -14,22 +14,58 @@ import Button from "@mui/material/Button";
 import { IconButton } from "@mui/material";
 import PaidIcon from "@mui/icons-material/Paid";
 import { removeToCart } from "../../redux/actions/products.actions";
-import {useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
+import { API_URL_BACKEND } from "../../api/apiRoute";
+import axios from "axios";
+import { useState } from "react";
 
 export default function ShoppingCart() {
   const dispatch = useDispatch();
   const items = useSelector((state) => state.productsReducer.cart);
+  const [redirect, setRedirect] = useState("");
+  let history = useHistory();
+
+  useEffect(() => {
+    if (redirect !== "") window.open(redirect,"_blank","popup=true");
+  }, [redirect]);
+
   let amount = 0;
   items && items.forEach((p) => (amount += Number(p.price)));
+  const cartId = 1;
+  
 
+  const handlePayment = async () => {
+    try {
+      await axios.delete(`${API_URL_BACKEND}cart/clearCart/${cartId}`);
 
-  let history = useHistory();
+      const serverPut = items.map((element) =>
+        axios.put(
+          `${API_URL_BACKEND}cart/addProductToCart/${cartId}/${element.id}`
+        )
+      );
+      Promise.all(serverPut)
+        .then((response) => {
+          console.log("response", response);
+          return response;
+        })
+        .then(() => axios.post(`${API_URL_BACKEND}shoppingOrders/${cartId}`))
+        .then((response) => {
+          console.log("response1", response);
+          return response;
+        })
+        .then((response) =>
+          axios.get(`${API_URL_BACKEND}payment?id=${response.data.id}`)
+        )
+        .then((response) => setRedirect(response.data.init_point))
+        .catch((error) => console.log("error del promiseall", error));
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
   function viewDetail(p) {
     history.push(`/juira/${p}`);
   }
-
-
 
   function handleRemoveinCart(p) {
     dispatch(removeToCart(p));
@@ -70,13 +106,23 @@ export default function ShoppingCart() {
                     <TableCell component="th" scope="row">
                       {i + 1}
                     </TableCell>
-                    <TableCell align="left" onClick={()=>{viewDetail(row.id)}}>
+                    <TableCell
+                      align="left"
+                      onClick={() => {
+                        viewDetail(row.id);
+                      }}
+                    >
                       <img
                         src={`${row.image}`}
                         style={{ maxHeight: "50px" }}
                       ></img>
                     </TableCell>
-                    <TableCell align="left" onClick={()=>{viewDetail(row.id)}} >
+                    <TableCell
+                      align="left"
+                      onClick={() => {
+                        viewDetail(row.id);
+                      }}
+                    >
                       <strong>{row.name}</strong>
                       <br></br>
                       <small>{row.description}</small>
@@ -100,7 +146,11 @@ export default function ShoppingCart() {
           </Table>
         </TableContainer>
       )}
-      <Button variant="contained" endIcon={<PaidIcon />}>
+      <Button
+        variant="contained"
+        endIcon={<PaidIcon />}
+        onClick={handlePayment}
+      >
         Pagar
       </Button>
     </Container>
