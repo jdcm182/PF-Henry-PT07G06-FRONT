d776519d527270f8aa475d8656b00f07d1894160
Jonatan P <jonatan2502@gmail.com>
2022-10-25 00:18:55 -0400

    fixed pagination

--- juira/src/components/Pagination/Pagination.jsx
+++ juira/src/components/Pagination/Pagination.jsx
@@ -2,16 +2,16 @@ import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 
-export default function PaginationButtons({ page, setPage, maxPage, products }) {
-  function handleChange(p) {
-    setPage(p)
+export default function PaginationButtons({page, setPage, maxPage, products }) {
+  function handleChange(page) {
+    setPage(page)
   }
   
-  // React.useEffect(() => {
-  //   setPage(1)
-  // },[products])
+  React.useEffect(() => {
+    setPage(1)
+  },[products])
 
   return (
-      <Pagination count={maxPage} onChange={(event, p) => handleChange(p)} defaultPage={1} showFirstButton showLastButton />
+      <Pagination count={maxPage} onChange={(event, page) => handleChange(page)} defaultPage={1} page={page} showFirstButton showLastButton />
   );
 }
