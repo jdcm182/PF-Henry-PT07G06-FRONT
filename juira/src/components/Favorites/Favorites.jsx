9bd623327dfa57163dfeb82872b863529b04b6f5
Jonatan P <jonatan2502@gmail.com>
2022-10-25 11:21:04 -0400

    Cambiado minHeight and agregado boton de volver al inicio

--- juira/src/components/Favorites/Favorites.jsx
+++ juira/src/components/Favorites/Favorites.jsx
@@ -1,21 +1,24 @@
-
-
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Typography, Container } from "@mui/material";
 import FavoriteDetail from "./FavoriteDetail";
+import HeartBrokenIcon from '@mui/icons-material/HeartBroken';
+import Button from "@mui/material/Button";
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import { useHistory } from "react-router-dom";
 
 export default function Favorites() {
 
   const items = useSelector((state) => state.productsReducer.favorites);
-
+  const history = useHistory();
 
   return (
-    <Container sx={{m:2}}>
+    <Container  sx={ {boxShadow: '0 0 15px 5px #cccccc55', padding: 5, width:'100'}}>
       <Typography variant="h4"> Favoritos </Typography>
       {!items.length ? (
-        <Container>
-          <Typography variant="h5"> No tienes favoritos aún </Typography>
+        <Container sx={{ minHeight: 350, margin: 2, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Typography variant="h5"> No tienes favoritos aún. </Typography>
+          <HeartBrokenIcon color='action' sx={{fontSize:'150px'}}/>
         </Container>
       ) : (
        <Container>
@@ -24,7 +27,19 @@ export default function Favorites() {
                         id={p.id} price={p.price} name={p.name} image={p.image} />
                 ))}
        </Container>
-  )
-}
-</Container>
+      )
+    } 
+      <Container sx={{margin: 2, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Button
+          variant="contained"
+          startIcon={<HomeRoundedIcon />}
+          sx={
+            {backgroundColor: '#23c197', '&:hover': {backgroundColor: '#138f6e'}}
+          }
+          onClick={()=>{history.push('/juira')}}
+          >
+          Inicio
+        </Button>
+      </Container>
+    </Container>
 )}
