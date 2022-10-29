3308e5ada322c72a860d6786a05c3c45446c56a9
Mariana Salez <mariana.i.salez@gmail.com>
2022-10-29 01:13:15 -0300

    login first approach

--- juira/src/components/Login/Login.jsx
+++ juira/src/components/Login/Login.jsx
@@ -1,16 +1,48 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 import { Grid,Paper, Avatar, TextField, Button, Typography } from '@mui/material';
 import LockIcon from '@mui/icons-material/Lock';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+// import firebase from 'firebase';
+import {getAuth} from 'firebase/auth'
 
 export default  function Login() {
+    const auth=getAuth()
+
+    const [user, setUser]=React.useState(null)
+
+    React.useEffect(()=>{
+        auth().onAuthStateChanged(user=>{
+            setUser(user)
+        })
+    },[])
+
+    const handleGoogleSignIn=()=>{
+        const provider= new auth.GoogleAeuthProvider()
+
+        auth().signInWithPopup(provider)
+        .then(result=>console.log(`${result.user.email} ha iniciado sesion`))
+        .catch(error=> console.log(`Error ${error.code}: ${error.message}`))
+    }
+
+    const handleLogOut=()=>{
+        auth().signOut()
+        .then(result=>console.log(`${result.user.email} ha salido`))
+        .catch(error=> console.log(`Error ${error.code}: ${error.message}`))
+
+    }
+
     const paperStyle={padding :20,height:'70vh',width:350, margin:"20px auto"}
     const avatarStyle={backgroundColor:'#1bbd7e'}
     const btnstyle={margin:'8px 0'}
     const txtstyle = { margin: '10px 0', fontFamily: 'nunito', color: 'var(--primaryColor)'}
     return(
+        user?
+        <div>
+            PERFIL DEL USUARIO
+            <Button onClick={handleLogOut}>Salir </Button>
+        </div>:
         <Grid>
             <Paper elevation={10} style={paperStyle} >
                 <Grid align='center' style={{marginBottom:'20px'}}>
@@ -38,6 +70,7 @@ export default  function Login() {
                         Olvidaste tu contraseña?
                 </Link>
                 </Typography> */}
+                <Button onClick={handleGoogleSignIn}>Login con Google</Button>
                 <Typography > Todavía no estás registrado?
                      <Link to="/juira/register" >
                         Regístrate 
