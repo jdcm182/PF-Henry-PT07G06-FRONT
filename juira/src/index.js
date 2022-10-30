ebe83aecd956984e88a8063028472f3150a38614
Mariana Salez <mariana.i.salez@gmail.com>
2022-10-29 22:19:41 -0300

    fix domain

--- juira/src/index.js
+++ juira/src/index.js
@@ -11,7 +11,7 @@ import { Toaster } from 'react-hot-toast';
 
 initializeApp({
 apiKey: "AIzaSyCkITagWMZ-q6tJCbyNu53nUxqD1cVJ-aY",
-authDomain: "juira-market-git-production-elehmann92.vercel.app/juira",
+authDomain: "juiraauth.firebaseapp.com",
 projectId: "juiraauth",
 storageBucket: "juiraauth.appspot.com",
 messagingSenderId: "449866263758",
