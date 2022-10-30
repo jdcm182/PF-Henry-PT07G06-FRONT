f584d27134029fc1bf5c862d3b2729fd1035ff0a
Mariana Salez <mariana.i.salez@gmail.com>
2022-10-29 22:36:52 -0300

    pruba ue

--- juira/src/components/Login/Login.jsx
+++ juira/src/components/Login/Login.jsx
@@ -32,10 +32,10 @@ const handleChange=(e)=>{
 
 
     const auth = getAuth();
-    
+    const user = auth.currentUser;
 
  React.useEffect(()=>{
-    const user = auth.currentUser;
+   user=auth.currentUser
  },[auth.currentUser])
 
 onAuthStateChanged(auth,(user)=>{
