9477d1b312ae4d04b0c4c08c93f4777435c7145d
Jonatan P <jonatan2502@gmail.com>
2022-11-01 22:36:42 -0400

    Fixed warning by removing unnecessary Typography tag

--- juira/src/components/DashboardAdmin/DashboardAdmin.jsx
+++ juira/src/components/DashboardAdmin/DashboardAdmin.jsx
@@ -26,7 +26,7 @@ function TabPanel(props) {
     >
       {value === index && (
         <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
+          {children}
         </Box>
       )}
     </div>
