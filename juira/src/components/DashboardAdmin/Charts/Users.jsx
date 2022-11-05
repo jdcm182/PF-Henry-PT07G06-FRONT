35ecd692d45a7c665d5c3a073efc9842378105e9
Jonatan P <jonatan2502@gmail.com>
2022-11-04 21:02:01 -0400

    Pie chart

--- juira/src/components/DashboardAdmin/Charts/Users.jsx
+++ juira/src/components/DashboardAdmin/Charts/Users.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { RadialBarChart, RadialBar, Legend, Tooltip } from "recharts";
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
 import axios from "axios";
 import { API_URL_BACKEND } from "../../../api/apiRoute";
 
@@ -33,52 +34,49 @@ export default function UsersChart() {
     const data = [
       {
         name: "Baneados",
-        Cantidad: bannedUsers,
+        value: bannedUsers,
         // pv: 2400,
         fill: "#8884d8"
       },
       {
         name: "Eliminados",
-        Cantidad: deletedUsers,
+        value: deletedUsers,
         // pv: 3908,
-        fill: "#ffc658"
+        fill: "#82ca9d",
       },
       {
         name: "Activos",
-        Cantidad: activeUsers,
+        value: activeUsers,
         // pv: 9800,
-        fill: "#82ca9d"
+        fill: "#ffc658"
       },
-      {
-        name: "Total",
-        Cantidad: totalUsers,
-        // pv: 9800,
-        fill: "#8dd1e1"
-      },
+      // {
+      //   name: "Total",
+      //   value: totalUsers,
+      //   // pv: 9800,
+      //   fill: "#8dd1e1"
+      // },
     ];
     
 
-
+    const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
   return (
-    <RadialBarChart
-      width={500}
-      height={300}
-      cx={150}
-      cy={150}
-      innerRadius={30}
-      outerRadius={140}
-      barSize={45}
+    <PieChart width={800} height={400}>
+    <Pie
       data={data}
+      cx={220}
+      cy={150}
+      innerRadius={40}
+      outerRadius={90}
+      fill="#8884d8"
+      paddingAngle={5}
+      dataKey="value"
     >
-      <Tooltip/>
-      <RadialBar
-        minAngle={15}
-        label={{ position: "insideStart", fill: "#fff" }}
-        background
-        clockWise
-        dataKey="Cantidad"
-      />
-      <Legend
+      {data.map((entry, index) => (
+        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+      ))}
+    </Pie>
+    <Legend
         iconSize={15}
         width={120}
         height={140}
@@ -86,6 +84,34 @@ export default function UsersChart() {
         verticalAlign="middle"
         wrapperStyle={style}
       />
-    </RadialBarChart>
+  </PieChart>
+    // <RadialBarChart
+    //   width={500}
+    //   height={300}
+    //   cx={150}
+    //   cy={150}
+    //   innerRadius={30}
+    //   outerRadius={140}
+    //   barSize={45}
+    //   data={data}
+    // >
+    //   <Tooltip/>
+    //   <RadialBar
+    //     minAngle={15}
+    //     label={{ position: "insideStart", fill: "#fff" }}
+    //     background
+    //     clockWise
+    //     dataKey="value"
+    //   />
+    //   <Legend
+    //     iconSize={15}
+    //     width={120}
+    //     height={140}
+    //     layout="vertical"
+    //     verticalAlign="middle"
+    //     wrapperStyle={style}
+    //   />
+    // </RadialBarChart>
+    
   );
 }
