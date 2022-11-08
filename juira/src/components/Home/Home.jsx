4b595112bb892d2ea99f38697a3493051d013ace
Mariana Salez <mariana.i.salez@gmail.com>
2022-11-08 20:13:30 -0300

    no Items in Category

--- juira/src/components/Home/Home.jsx
+++ juira/src/components/Home/Home.jsx
@@ -15,7 +15,9 @@ import {
 import FilterBar from "../FilterBar/FilterBar";
 import { useLocation } from "react-router-dom";
 import { updateFilter } from "../../redux/actions/app.actions";
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import { useHistory } from 'react-router-dom';
 
 const _ = require("lodash");
 
@@ -26,6 +28,7 @@ function useQuery() {
 }
 
 export default function Home(/* { prods, getAll } */) {
+  const history = useHistory();
   let query = useQuery();
   const productsState = useSelector(
     (state) => state.productsReducer.allProducts
@@ -111,7 +114,23 @@ export default function Home(/* { prods, getAll } */) {
           <Sidebar />
         </div> */}
         <div className={styles.cardsGridContainer}>
-          <CardsGrid products={stateFilterAndSort} />
+          {(stateFilterAndSort.length>0)?
+          <CardsGrid products={stateFilterAndSort} />:
+          <Container sx={{display: 'flex', flexDirection: 'column' ,alignContent: 'center' , justifyContent:'center'}}>
+            <Typography sx={{m:2, textAlign: 'center'}}>
+            No se encontraron productos segun su busqueda
+          </Typography>
+          <Button  variant="contained"
+          sx={
+            {backgroundColor: '#23c197', '&:hover': {backgroundColor: '#138f6e'}, mb:3, width:0.2, alignSelf:'center'}
+          }
+          onClick={()=>{ dispatch(updateFilter({ name: "categories", value: "Todos" }))}}>Volver</Button>
+
+          </Container>
+         
+        
+        }
+          
         </div>
       </div>
     </section>
